f8fb42f868a74ad203f82bd35acfd721e405edd0
ADE YOGA WIYADNYANA <yorisofficial@gmail.com>
2021-11-20 18:41:17 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// music togle
 var mysong = document.getElementById("mysong");
 var icon = document.getElementById("icon");
 
@@ -11,6 +12,35 @@ icon.onclick = function(){
     } 
 };
 
+// count-down
+var countDownDate = new Date("1 1, 2022 13:54:00").getTime();
+
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("Days").innerHTML = days + "d ";
+  document.getElementById("Hours").innerHTML = hours + "h ";
+  document.getElementById("Minutes").innerHTML = minutes + "m ";
+  document.getElementById("Seconds").innerHTML = seconds + "s ";
+  
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("Days").innerHTML = "- -";
+    document.getElementById("Hours").innerHTML = "- -";
+    document.getElementById("Minutes").innerHTML = "- -";
+    document.getElementById("Seconds").innerHTML = "- -";
+  }
+}, 1000);
+
+// buku tamu
 const scriptURL = 'https://script.google.com/macros/s/AKfycbxSmcwgAdOpNi9amAbQMQDmHZAZkVtsNC4f2P7e82HutU5x6fS6DeNmZJ5R-_YvXzh9xA/exec'
 const form = document.forms['wpp-template']
 const btnKirim = document.querySelector('.btn-kirim')
